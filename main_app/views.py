c04cabfa20357fcf77d81c806f60156415fdba94
Jared Siemen <siemenjm@gmail.com>
2022-09-30 16:51:37 -0400

    Render data in Institution show page using fake data

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,25 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = 'about.html'
+
+# Add Institution Class for mock data
+class Institution:
+    def __init__(self, institution_id, name, logo):
+        self.institution_id = institution_id
+        self.name = name
+        self.logo = logo
+
+# Fake Institution Data
+institutions = [
+    Institution('ins_1', 'Chase Bank', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtaSOK8ycH6cN56fkzGoyUW30utQ71XiGx0Q&usqp=CAU'),
+    Institution('ins_2', 'Bank of America', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ57oF7N_QozC50QZCwHL_UhoNBxbw-Ldlgg&usqp=CAU'),
+    Institution('ins_3', 'PNC Bank', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjPq4L0RWsvCEIXJcvMJHurh4eyC03y2VTpQ&usqp=CAU')
+]
+
+class InstitutionList(TemplateView):
+    template_name = 'institution_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["institutions"] = institutions # this is where we add the key into our context object for the view to use
+        return context

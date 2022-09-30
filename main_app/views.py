152c28797c338b08607b9dd62405b7267bbc4b11
Jared Siemen <siemenjm@gmail.com>
2022-09-30 16:01:11 -0400

    Add Home and About templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse('Budget Home')
+class Home(TemplateView):
+    template_name = 'home.html'
 
-class About(View):
-    def get(self, request):
-        return HttpResponse('Budget About')
+class About(TemplateView):
+    template_name = 'about.html'

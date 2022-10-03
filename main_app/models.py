6b26f2d8cfde22917acf3cb656062d4e423b5ef6
Jared Siemen <siemenjm@gmail.com>
2022-10-03 10:10:12 -0400

    Add Institution model and migrate to psql db. Create superuser.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Institution(models.Model):
+    institution_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=250)
+    logo = models.URLField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['institution_id']

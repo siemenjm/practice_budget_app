4e094d723b52e42fc2e9c0c014d0fff4ce61bda7
Jared Siemen <siemenjm@gmail.com>
2022-10-03 10:49:04 -0400

    Update InistitutionList to use db models and add search funtion.

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Institution
 
 class Home(TemplateView):
     template_name = 'home.html'
@@ -9,24 +10,32 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = 'about.html'
 
-# Add Institution Class for mock data
-class Institution:
-    def __init__(self, institution_id, name, logo):
-        self.institution_id = institution_id
-        self.name = name
-        self.logo = logo
+# # Add Institution Class for mock data
+# class Institution:
+#     def __init__(self, institution_id, name, logo):
+#         self.institution_id = institution_id
+#         self.name = name
+#         self.logo = logo
 
-# Fake Institution Data
-institutions = [
-    Institution('ins_1', 'Chase Bank', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtaSOK8ycH6cN56fkzGoyUW30utQ71XiGx0Q&usqp=CAU'),
-    Institution('ins_2', 'Bank of America', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ57oF7N_QozC50QZCwHL_UhoNBxbw-Ldlgg&usqp=CAU'),
-    Institution('ins_3', 'PNC Bank', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjPq4L0RWsvCEIXJcvMJHurh4eyC03y2VTpQ&usqp=CAU')
-]
+# # Fake Institution Data
+# institutions = [
+#     Institution('ins_1', 'Chase Bank', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtaSOK8ycH6cN56fkzGoyUW30utQ71XiGx0Q&usqp=CAU'),
+#     Institution('ins_2', 'Bank of America', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ57oF7N_QozC50QZCwHL_UhoNBxbw-Ldlgg&usqp=CAU'),
+#     Institution('ins_3', 'PNC Bank', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjPq4L0RWsvCEIXJcvMJHurh4eyC03y2VTpQ&usqp=CAU')
+# ]
 
 class InstitutionList(TemplateView):
     template_name = 'institution_list.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["institutions"] = institutions # this is where we add the key into our context object for the view to use
+
+        name = self.request.GET.get('name')
+        if name != None:
+            context['institutions'] = Institution.objects.filter(name__icontains=name)
+            context['header'] = f'Searching for {name}'
+        else:
+            context["institutions"] = Institution.objects.all()
+            context['header'] = 'Your Institutions'
+
         return context

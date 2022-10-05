ff78ace0a383cc7a5be4e2956e3e64ebc6a1c53f
Jared Siemen <siemenjm@gmail.com>
2022-10-05 16:42:27 -0400

    Add user authorization to protect routes

--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,8 @@ from django.urls import reverse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class Home(TemplateView):
     template_name = 'home.html'
@@ -16,6 +18,7 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = 'about.html'
 
+@method_decorator(login_required, name='dispatch')
 class InstitutionList(TemplateView):
     template_name = 'institution_list.html'
 
@@ -32,6 +35,7 @@ class InstitutionList(TemplateView):
 
         return context
 
+@method_decorator(login_required, name='dispatch')
 class InstitutionCreate(CreateView):
     model = Institution
     fields = ['institution_id', 'name', 'logo']
@@ -42,10 +46,12 @@ class InstitutionCreate(CreateView):
         form.instance.user = self.request.user
         return super(InstitutionCreate, self).form_valid(form)
 
+@method_decorator(login_required, name='dispatch')
 class InstitutionDetail(DetailView):
     model = Institution
     template_name = 'institution_detail.html'
 
+@method_decorator(login_required, name='dispatch')
 class InstitutionUpdate(UpdateView):
     model = Institution
     fields = ['institution_id', 'name', 'logo']
@@ -54,11 +60,13 @@ class InstitutionUpdate(UpdateView):
     def get_success_url(self):
         return reverse('institution_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required, name='dispatch')
 class InstitutionDelete(DeleteView):
     model = Institution
     template_name = 'institution_delete_confirmation.html'
     success_url = '/institutions/'
 
+@method_decorator(login_required, name='dispatch')
 class AccountList(TemplateView):
     template_name = 'account_list.html'
 
@@ -70,6 +78,7 @@ class AccountList(TemplateView):
 
         return context
 
+@method_decorator(login_required, name='dispatch')
 class AccountCreate(CreateView):
     model = Account
     fields = ['account_id', 'balance_available', 'balance_current', 'name', 'account_type', 'account_subtype', 'institution']
@@ -80,10 +89,12 @@ class AccountCreate(CreateView):
         form.instance.user = self.request.user
         return super(AccountCreate, self).form_valid(form)
 
+@method_decorator(login_required, name='dispatch')
 class AccountDetail(DetailView):
     model = Account
     template_name = 'account_detail.html'
 
+@method_decorator(login_required, name='dispatch')
 class AccountUpdate(UpdateView):
     model = Account
     fields = ['account_id', 'balance_available', 'balance_current', 'name', 'account_type', 'account_subtype', 'institution']
@@ -92,11 +103,13 @@ class AccountUpdate(UpdateView):
     def get_success_url(self):
         return reverse('account_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required, name='dispatch')
 class AccountDelete(DeleteView):
     model = Account
     template_name = 'account_delete_confirmation.html'
     success_url = '/accounts/'
 
+@method_decorator(login_required, name='dispatch')
 class TransactionList(TemplateView):
     template_name = 'transaction_list.html'
 
@@ -108,6 +121,7 @@ class TransactionList(TemplateView):
 
         return context
 
+@method_decorator(login_required, name='dispatch')
 class TransactionCreate(CreateView):
     model = Transaction
     fields = ['amount', 'description', 'category', 'sub_category', 'sub_sub_category', 'debited_account', 'credited_account', 'transaction_date']
@@ -118,10 +132,12 @@ class TransactionCreate(CreateView):
         form.instance.user = self.request.user
         return super(TransactionCreate, self).form_valid(form)
 
+@method_decorator(login_required, name='dispatch')
 class TransactionDetail(DetailView):
     model = Transaction
     template_name = 'transaction_detail.html'
 
+@method_decorator(login_required, name='dispatch')
 class TransactionUpdate(UpdateView):
     model = Transaction
     fields = ['amount', 'description', 'category', 'sub_category', 'sub_sub_category', 'debited_account', 'credited_account', 'transaction_date']
@@ -130,6 +146,7 @@ class TransactionUpdate(UpdateView):
     def get_success_url(self):
         return reverse('transaction_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required, name='dispatch')
 class TransactionDelete(DeleteView):
     model = Transaction
     template_name = 'transaction_delete_confirmation.html'
